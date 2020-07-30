4f7c566540eeb0aeedb2bc27de7ed032eed318a0
sgwon96 <zxcvb5434@likelion.org>
2020-07-31 04:23:28 +0900

    first setting

--- src/api/Hello/sayHello/sayHello.js
+++ src/api/Hello/sayHello/sayHello.js
@@ -1,5 +1,11 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+
 export default {
    Query: {
-     sayHello: () => "Bai"
+     sayHello: async () => {
+       console.log(await prisma.users());
+       return "HELLO";
+     }
    }
  };

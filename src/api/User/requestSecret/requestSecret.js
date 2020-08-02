125c8a5a6f384d507adedf3baf86ec910f78b7bf
sgwon96 <zxcvb5434@likelion.org>
2020-08-03 06:09:47 +0900

    PassportJWT

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -1,14 +1,15 @@
-import { generateSecret } from "../../../utils";
- import { prisma } from "../../../../generated/prisma-client";
+import { generateSecret, sendSecretMail } from "../../../utils";
+import { prisma } from "../../../../generated/prisma-client";
 
  export default {
    Mutation: {
-     requestSecret: async (_, args) => {
+     requestSecret: async (_, args, { request }) => {
        const { email } = args;
        const loginSecret = generateSecret();
        try {
+         throw Error();
+         await sendSecretMail(email, loginSecret);
          await prisma.updateUser({ data: { loginSecret }, where: { email } });
-         console.log(loginSecret);
          return true;
        } catch (error) {
          console.log(error);

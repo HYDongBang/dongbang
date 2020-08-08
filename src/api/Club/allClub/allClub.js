411ae6bc023a252af489b230ee1ac624fc5a2de3
이도현 <leedh2008@naver.com>
2020-08-08 18:23:25 +0900

    동아리 목록보기 #3

--- src/api/Club/allClub/allClub.js
+++ src/api/Club/allClub/allClub.js
@@ -2,9 +2,7 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
     Query: {
-        allClub: async () => {
-            console.log(await prisma.clubs());
-            return "HELLO";
-        }
+        allClub: async () => prisma.clubs()
+        //$fragment()
     }
 }
